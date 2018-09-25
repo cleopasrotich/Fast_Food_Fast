7c6f6fcc808a61c366c0dcd6ba4c50f148d5f264
cleopasrotich <cleopasrotich81@gmail.com>
2018-09-25 14:33:37 +0300

    changed directory of css and images

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+var request = new XMLHttpRequest();
+
+request.open('GET', 'https://127.0.0.1:5000/Fast_Food_Fast/api/v1/orders', true);
+
+request.onload = function(){
+
+};
+
+request.send();
+
+
+
+
